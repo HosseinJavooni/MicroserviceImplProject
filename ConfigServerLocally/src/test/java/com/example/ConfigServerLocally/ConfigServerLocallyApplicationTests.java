e2a37d286fc7e9c455bfb957c2bb910bb6aa5e31
HosseinJavooni <h.javooni@gmail.com>
2023-11-24 15:31:45 +0330

    2023/11/24_15:31pm

---
+++ ConfigServerLocally/src/test/java/com/example/ConfigServerLocally/ConfigServerLocallyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ConfigServerLocally;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerLocallyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
